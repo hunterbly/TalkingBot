a9354fc8d862cf163c84ef25561652189585d967
Billy Lam <0chun0@gmail.com>
2020-01-11 23:39:39 +0800

    chagne endpoing to variable

--- src/hotdog.py
+++ src/hotdog.py
@@ -26,11 +26,14 @@ def testing():
 def postit(method):
     def posted(*args, **kw):
         func_name = method(*args, **kw)
-
-        url = "http://206.189.149.240:4000/ocpu/library/HotDog/R/get_signal_performance/json"
-        payload = {'code': '1'}
+        url = "http://{}:{}/ocpu/library/{}/R/{}/json".format(CONST_ENDPOINT,
+                                                              CONST_PORT,
+                                                              CONST_LIBRARY,
+                                                              func_name)
+        payload = kw
         headers = {}
 
+        # Send request to R OpenCPU server
         response = requests.request("POST", url, headers=headers, data = payload)
         res = response.text.encode('utf8')
 
