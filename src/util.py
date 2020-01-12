82916720347f36a528f1d50a4302d7c828f6de8a
Billy Lam <0chun0@gmail.com>
2020-01-12 22:04:20 +0800

    Add logic for parse_df

--- src/util.py
+++ src/util.py
@@ -2,3 +2,10 @@ from tabulate import tabulate
 
 def util_test():
     return("utility")
+
+def parse_df(df):
+
+    ## parse dataframe to tabula format to be displayed in telegram
+
+    res = tabulate(df, tablefmt = "psql", headers="keys", showindex="never")
+    return(res)
