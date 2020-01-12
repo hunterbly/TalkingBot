785a429e9afb7a4a6e50660d6f628b94b10847d9
Billy Lam <0chun0@gmail.com>
2020-01-12 22:34:57 +0800

    Test user input

--- talking_bot.py
+++ talking_bot.py
@@ -44,43 +44,25 @@ def start(bot, update):
     update.message.reply_text('Test Hi!')
 
 def test(bot, update):
-    res = load_hit_signal(ref_date = '2020-01-10')
-    print(res)
+
+    # Try parsing user input 
+    input_date = update.message.text
+    print(input_date)
+
+    # res = load_hit_signal(ref_date = '2020-01-10')
+    # print(res)
     
-#   update.message.reply_text(res.shape[0])
-#   update.message.reply_text(res.style.render(), parse_mode='HTML')
-    some_html = """<html><style  type="text/css" >
-</style><table id="T_8c339870_34f3_11ea_adac_0242ac110006" ><thead>    <tr>        <th class="blank level0" ></th>        <th class="col_heading level0 col0" >code</th>        <th class="col_heading level0 col1" >date</th>        <th class="col_heading level0 col2" >signal</th>        <th class="col_heading level0 col3" >hit</th>    </tr></thead><tbody>
-                <tr>
-                        <th id="T_8c339870_34f3_11ea_adac_0242ac110006level0_row0" class="row_heading level0 row0" >0</th>
-                        <td id="T_8c339870_34f3_11ea_adac_0242ac110006row0_col0" class="data row0 col0" >1918</td>
-                        <td id="T_8c339870_34f3_11ea_adac_0242ac110006row0_col1" class="data row0 col1" >2020-01-10 00:00:00</td>
-                        <td id="T_8c339870_34f3_11ea_adac_0242ac110006row0_col2" class="data row0 col2" >s_bear_stick</td>
-                        <td id="T_8c339870_34f3_11ea_adac_0242ac110006row0_col3" class="data row0 col3" >1</td>
-            </tr>
-    </tbody></table>  
-</html>"""
-    update.message.reply_text(some_html, parse_mode='HTML')
+    update.message.reply_text(date)
 
 def table(bot, update):
 
    df = load_hit_signal(ref_date = '2020-01-10')
+   df.drop(columns=['date'], axis = 1, inplace=True, errors='ignore')  # drop id column if exists
+
    table_html = parse_df(df)
 
    update.message.reply_text(table_html, parse_mode='HTML')
 
-def table2(bot, update):
-    html = """
-    <table>
-    <tbody>
-    <tr><th>item  </th><th style="text-align: right;">  qty</th></tr>
-    <tr><td>spam  </td><td style="text-align: right;">   42</td></tr>
-    <tr><td>eggs  </td><td style="text-align: right;">  451</td></tr>
-    <tr><td>bacon </td><td style="text-align: right;">    0</td></tr>
-    </tbody>
-    </table>
-    """
-    update.message.reply_text(html, parse_mode='HTML')
    
 def help(bot, update):
     """Send a message when the command /help is issued."""
@@ -118,7 +100,6 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("test", test))
     dp.add_handler(CommandHandler("table", table))
-    dp.add_handler(CommandHandler("table2", table2))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("lunch", lunch))
 
