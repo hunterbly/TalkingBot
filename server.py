4666fd6387ea812dc757fb8839059fb7e633b3ab
Billy Lam <0chun0@gmail.com>
2020-02-02 23:15:22 +0800

    Add checking if return is dataframe

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from src.hotdog import *
 from src.util import *
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ChatAction, ParseMode
+import pandas as pd
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -66,6 +67,10 @@ def dummy(update, context):
 
     update.message.reply_text(df_str, parse_mode = ParseMode.HTML)
 
+#####################
+# Real commands     #  
+#####################
+
 @typing    
 def signal(update, context):
 
@@ -77,33 +82,38 @@ def signal(update, context):
         input_str = None
 
     ref_date_str = format_input_date(input_str)
-        
-    # Create signal mapping dataframe
-    mapping = [['s_bull_stick', "\u5927\u967d\u71ed"],
-               ['s_bear_stick', "\u5927\u9670\u71ed"],
-               ['s_bull_engulf', "\u5411\u597d\u541e\u566c"],
-               ['s_bear_engulf', "\u5411\u6de1\u541e\u566c"],
-               ['s_bull_harami', "\u5411\u597d\u8eab\u61f7\u516d\u7532"],
-               ['s_bear_harami', "\u5411\u6de1\u8eab\u61f7\u516d\u7532"],
-               ['s_2day_reverse_good', "\u5411\u597d\u96d9\u65e5\u8f49\u5411"],
-               ['s_2day_reverse_bad', "\u5411\u6de1\u96d9\u65e5\u8f49\u5411"],
-               ['s_bull_pierce', "\u66d9\u5149\u521d\u73fe"],
-               ['s_bear_pierce', "\u70cf\u96f2\u84cb\u9802"],
-               ['s_hammer', "\u939a\u982d"],
-               ['s_shooting_star', "\u5c04\u64ca\u4e4b\u661f"]] 
-    df_map  = pd.DataFrame(mapping, columns = ['signal', 'signal_label'])
-
-    # Load hit signal, map signal label
     df_signal = load_hit_signal(ref_date = ref_date_str)
-    df_res = df_signal.merge(df_map, on='signal', how='left')
-    df_res = df_res[['code', 'date', 'signal_label']]
-    
-    # Print dataframe
-    df_str = print_df(df_res)
-    
-    update.message.reply_text(df_str, parse_mode = ParseMode.HTML)
 
+    # If dataframe, else return error message
+    if isinstance(df_signal, pd.DataFrame):
+        # Create signal mapping dataframe
+        mapping = [['s_bull_stick', "\u5927\u967d\u71ed"],
+                   ['s_bear_stick', "\u5927\u9670\u71ed"],
+                   ['s_bull_engulf', "\u5411\u597d\u541e\u566c"],
+                   ['s_bear_engulf', "\u5411\u6de1\u541e\u566c"],
+                   ['s_bull_harami', "\u5411\u597d\u8eab\u61f7\u516d\u7532"],
+                   ['s_bear_harami', "\u5411\u6de1\u8eab\u61f7\u516d\u7532"],
+                   ['s_2day_reverse_good', "\u5411\u597d\u96d9\u65e5\u8f49\u5411"],
+                   ['s_2day_reverse_bad', "\u5411\u6de1\u96d9\u65e5\u8f49\u5411"],
+                   ['s_bull_pierce', "\u66d9\u5149\u521d\u73fe"],
+                   ['s_bear_pierce', "\u70cf\u96f2\u84cb\u9802"],
+                   ['s_hammer', "\u939a\u982d"],
+                   ['s_shooting_star', "\u5c04\u64ca\u4e4b\u661f"]] 
+        df_map  = pd.DataFrame(mapping, columns = ['signal', 'signal_label'])
 
+        # Load hit signal, map signal label
+        df_res = df_signal.merge(df_map, on='signal', how='left')
+        df_res = df_res[['code', 'date', 'signal_label']]
+
+        # Print dataframe
+        df_str = print_df(df_res)
+
+        update.message.reply_text(df_str, parse_mode = ParseMode.HTML)
+
+    else:
+        err = df_signal.decode("utf-8")
+        update.message.reply_text(err, parse_mode = ParseMode.HTML)
+        
 @typing    
 def hello(update, context):
 
