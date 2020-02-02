7bb5c8b912a923c765900fc24199f35b82ede949
Billy Lam <0chun0@gmail.com>
2020-02-02 18:43:30 +0800

    Init get_input_date

--- src/util.py
+++ src/util.py
@@ -59,3 +59,33 @@ def print_df(df, bold = None):
     res = '\n'.join(msg)
     
     return(res)
+
+def get_input_date(str):
+    print(str)
+    # Try parsing user input 
+
+    # input_str = update.message.text
+
+    # if(len(input_str.split()) > 1):
+    #    input_str = input_str.split()[1]  # First argument
+       
+    # else:
+    #    input_str = date.today().strftime("%Y%m%d")
+       
+    # try:
+    #    ref_date = datetime.strptime(input_str, '%Y%m%d')
+    #    ref_str = ref_date.strftime('%Y-%m-%d')
+       
+    #    df = load_hit_signal(ref_date = ref_str)
+
+    #    if isinstance(df, pd.DataFrame):
+    #        table_html = parse_df(df)
+    #        update.message.reply_text(table_html, parse_mode='HTML')
+    #    else:
+    #        err_msg = df.decode("utf-8")
+    #        update.message.reply_text(err_msg)
+       
+    # except Exception as e:
+    #     print(e)
+    #     error = 'Please input date in YYYYMMDD format'
+    #     update.message.reply_text(error)
