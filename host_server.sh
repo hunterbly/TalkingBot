b004c2dbcdd4114668496dc3a48e2db32ec5d8bf
Billy Lam <0chun0@gmail.com>
2020-05-17 20:16:04 +0800

    Add host_server sh

---
+++ host_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+usage ()
+{
+    echo 'Usage: ./host_server.sh'
+
+    exit
+}
+
+echo ""
+echo "New script, use with caution"
+echo ""
+
+branch = "master"
+
+git fetch
+git checkout $branch
+git pull
+
+echo ""
+echo "Done - Checked out latest $branch branch"
+echo ""
+
+python server.py
