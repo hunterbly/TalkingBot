532a53875a053d54b42f5d6893f2a424dc85177e
Billy Lam <0chun0@gmail.com>
2020-05-18 22:59:20 +0800

    Init send telegram message

---
+++ src/util_telegram.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def send_telegram_message(msg):
+
+    """ Utility to send message to telegram groups """
+
+    base_url = "https://api.telegram.org/bot589160362:AAHEeNBIeh3m3RA07lANaDHovy874xNFi1g/sendMessage"
+    chat_id_one = "-464227511"
+    chat_id_two = "-464227511"
+    msg = msg
+
+    url_one = f"{base_url}?chat_id={chat_id_one}&text={msg}"
+    url_two = f"{base_url}?chat_id={chat_id_two}&text={msg}"
+
+    payload = {}
+    headers = {}
+
+    # Send message to both groups
+    requests.request("POST", url_one, headers=headers, data=payload)
+    requests.request("POST", url_two, headers=headers, data=payload)
+
+    return(None)
+
+
+if __name__ == '__main__':
+    send_telegram_message("ABC DEF")
