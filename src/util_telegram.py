016b463fb5ef48603e7ec22a2673c8e42ee57687
Billy Lam <0chun0@gmail.com>
2020-05-18 23:05:32 +0800

    Add send message to the other group as well

--- src/util_telegram.py
+++ src/util_telegram.py
@@ -6,8 +6,8 @@ def send_telegram_message(msg):
     """ Utility to send message to telegram groups """
 
     base_url = "https://api.telegram.org/bot589160362:AAHEeNBIeh3m3RA07lANaDHovy874xNFi1g/sendMessage"
-    chat_id_one = "-464227511"
-    chat_id_two = "-464227511"
+    chat_id_one = "-294841384"  # Signal Testing
+    chat_id_two = "-464227511"  # Signal Development
     msg = msg
 
     url_one = f"{base_url}?chat_id={chat_id_one}&text={msg}"
