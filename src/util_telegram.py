683a043af91909728c39eb949d90af55be7c6475
Billy Lam <0chun0@gmail.com>
2020-05-27 00:37:10 +0800

    Small change

--- src/util_telegram.py
+++ src/util_telegram.py
@@ -6,7 +6,7 @@ def send_telegram_message(msg):
     """ Utility to send message to telegram groups """
 
     base_url = "https://api.telegram.org/bot589160362:AAHEeNBIeh3m3RA07lANaDHovy874xNFi1g/sendMessage"
-    # chat_id_one = "-294841384"  # Signal Testing
+    chat_id_one = "-294841384"  # Signal Testing
     chat_id_two = "-464227511"  # Signal Development
     msg = msg
 
@@ -17,7 +17,7 @@ def send_telegram_message(msg):
     headers = {}
 
     # Send message to both groups
-    requests.request("POST", url_one, headers=headers, data=payload)
+    # requests.request("POST", url_one, headers=headers, data=payload)
     requests.request("POST", url_two, headers=headers, data=payload)
 
     return(None)
